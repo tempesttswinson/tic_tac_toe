0674efcb65ea3330a530e534a417de83667c55b1
tempesttswinson <tempestt.swinson@gmail.com>
2020-04-27 16:28:58 -0400

    Part 2 of tic tac toe

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,12 +2,12 @@ import React, { useState } from 'react';
 
 export default function Square(props) {
 
-    const [value, setValue] = useState(null);
-
     return (
-        <button className='square'
-            onClick={() => setValue('X')}>
-            {value}
+        <button
+            className='square'
+            onClick={props.onClick}
+        >
+            {props.value}
         </button>
     )
 }
